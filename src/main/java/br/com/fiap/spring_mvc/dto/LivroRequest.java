7a2271a9cbaa82645788a6bfc31c5e668f807c8d
Luiz Gustavo S. Real <lgsreal2@gmail.com>
2025-02-25 09:54:07 -0300

    aula 25-02-2025

--- src/main/java/br/com/fiap/spring_mvc/dto/LivroRequest.java
+++ src/main/java/br/com/fiap/spring_mvc/dto/LivroRequest.java
@@ -3,7 +3,6 @@ package br.com.fiap.spring_mvc.dto;
 import br.com.fiap.spring_mvc.model.Categoria;
 import jakarta.validation.constraints.*;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 
